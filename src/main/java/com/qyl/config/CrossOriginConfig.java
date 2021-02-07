765728e2600f330ff41d922bbe2757deea9293e1
QiuYiliang <1162922438@qq.com>
2021-02-07 17:14:16 +0800

    解决跨域问题

---
+++ src/main/java/com/qyl/config/CrossOriginConfig.java
@@ -0,0 +1,28 @@
+package com.qyl.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @Author: qyl
+ * @Date: 2021/2/7 13:02
+ * @Description:
+ */
+@Configuration
+public class CrossOriginConfig {
+
+    @Bean
+    public WebMvcConfigurer webMvcConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedHeaders("*")
+                        .allowedMethods("*")
+                        .allowedOrigins("*");
+            }
+        };
+    }
+}
