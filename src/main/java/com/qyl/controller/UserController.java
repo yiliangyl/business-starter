c7e5adc5f79a564f756abe55edad21333acf6a07
QiuYiliang <1162922438@qq.com>
2021-02-23 11:02:15 +0800

    修改字段

--- src/main/java/com/qyl/controller/UserController.java
+++ src/main/java/com/qyl/controller/UserController.java
@@ -51,7 +51,6 @@ public class UserController {
      * 发送验证码
      * @param phone
      * @return
-     * @apiNote 测试阶段
      */
     @PostMapping("/send")
     public ResponseEntity<String> sendVerificationCode(
