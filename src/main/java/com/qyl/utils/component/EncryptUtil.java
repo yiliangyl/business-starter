2e429a6df52391d0a899ac8b238c7ea2d99f33a6
QiuYiliang <1162922438@qq.com>
2021-03-16 18:47:27 +0800

    修改加密工具类

--- src/main/java/com/qyl/utils/component/EncryptUtil.java
+++ src/main/java/com/qyl/utils/component/EncryptUtil.java
@@ -1,10 +1,6 @@
 package com.qyl.utils.component;
 
-import org.apache.tomcat.util.security.MD5Encoder;
-
-import java.nio.charset.StandardCharsets;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+import org.springframework.util.DigestUtils;
 
 /**
  * 数据加密工具类
@@ -14,20 +10,16 @@ import java.security.NoSuchAlgorithmException;
 public class EncryptUtil {
 
     /**
-     * 数据加密
+     * 随机盐
+     */
+    private static final String SALT = "$R67@EdKd7^%PLw7";
+
+    /**
+     * MD5 加密
      * @param origin 原始数据
      * @return 加密数据
      */
     public static String encryptByMD5(String origin) {
-        String encrypt = "";
-        try {
-            // 确定计算方法
-            MessageDigest md5 = MessageDigest.getInstance("MD5");
-            // 通过MD5加密
-            encrypt = MD5Encoder.encode(md5.digest(origin.getBytes(StandardCharsets.UTF_8)));
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
-        return encrypt;
+        return DigestUtils.md5DigestAsHex((origin + SALT).getBytes());
     }
 }
